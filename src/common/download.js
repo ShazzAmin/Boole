4ca29e5a6a3b9784c0f750d72dfc8fbca0cbf902
Daniel Zhang <danielzhang130@gmail.com>
2019-09-08 20:59:36 -0400

    Fixed formatting issue.

--- src/common/download.js
+++ src/common/download.js
@@ -2,7 +2,7 @@ export const download = (fileName, value) => {
   const element = document.createElement("a");
   element.target = "_blank";
   element.download = fileName;
-  element.href = `data:text/plain,charset=utf-8,${encodeURIComponent(value)}`;
+  element.href = `data:text/plain;charset=utf-8,${encodeURIComponent(value)}`;
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
