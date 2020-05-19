36a90c18ae85659b47a2dd414742c46ef005c0a5
Shazz Amin <me@shazz.me>
2020-05-18 23:12:43 -0400

    Enhancement: Fail gracefully when fail to fetch remote files (#32)
    * Update dependency
    
    * Fail gracefully when fail to fetch remote files
    
    * Add newline at end of file

---
+++ src/common/misc-files.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		name: "scratchpad.grg",
+		contents: `#u name
+#a 00
+
+#q 00
+
+#check PROP
+
+a => b`,
+	},
+	{
+		name: "nd_example.grg",
+		contents: `#u name
+#a 00
+#q 00
+
+#check ND
+
+p => !q, r => q |- p => !r
+
+1) p => !q premise
+2) r => q premise
+3) assume p {
+	4) !q by imp_e on 1, 3
+	5) !r by imp_e on 2, 4
+}
+6) p => !r by imp_i on 3-5`,
+	}
+];
+
